b296d23485810fde0e73ba3ce04cf0df991dada4
carolinab-png <carolinabarrosadvocacia@gmail.com>
2025-08-15 17:00:55 -0300

    discovery the area

---
+++ area.py
@@ -0,0 +1,11 @@
+n1 = input("altura do retângulo:")
+
+n2 = input("largura do retângulo:")
+
+altura = int(n1)
+
+largura = int(n2)
+
+área = altura *largura
+
+print(f"a área do retângulo é {área}") 
