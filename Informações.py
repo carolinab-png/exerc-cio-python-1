769c3c4b859b6e6dbcf4f5c82a105fb3b88a4f1a
carolinab-png <carolinabarrosadvocacia@gmail.com>
2025-08-15 16:12:06 -0300

    create a repository

---
+++ Informações.py
@@ -0,0 +1,8 @@
+nome_produto= "livro"
+preco_produto= 20.00
+quantidade_estoque= int(40)
+print(f"Produto: {nome_produto}")
+
+print(f"Preço: R$ {preco_produto}")
+
+print(f"Quantidade em estoque: {quantidade_estoque}")
