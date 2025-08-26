f8c6aa14a903bdbf357017dc6e167bf9c39e7534
carolinab-png <carolinabarrosadvocacia@gmail.com>
2025-08-26 20:19:02 -0300

    editado para o desafio 100 dias com Angela

--- atividade2/atv2q2.py
+++ atividade2/atv2q2.py
@@ -1,9 +1,7 @@
-# Seu código para obter o número e calcular o resto da divisão
 numero = int(input("digite o número"))
 resultado = numero % 2
 
-# Aqui você vai usar o if-else
 if resultado == 0:
-    print("par")  # Apenas a linha 'print' precisa de indentação
+    print("par")  
 else:
-    print("impar") # E a linha 'print' do else também
+    print("impar") 
